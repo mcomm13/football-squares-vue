ac6b054b31fb543b0379556c6d40fb18a391e55f
Mike Commendatore <michael.commendatore@outlook.com>
2019-12-23 12:24:20 -0500

    Commit of first working iteration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">Football Squares Number Picker</div>
+      <v-spacer></v-spacer>
+      <v-btn href="https://github.com/mcomm13/football-squares-vue" target="_blank" text>
+        <span class="mr-2">View on GitHub</span>
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <FootballSquares
+        v-bind:numberPairs="numberPairs"
+        v-on:assign-numbers="numberPairs = assignNumberPairs()"
+      />
+    </v-content>
+    <v-footer>&copy; 2019 Michael Commendatore</v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FootballSquares from "./components/FootballSquares";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    FootballSquares
+  },
+
+  data: () => {
+    return {
+      numberPairs: [
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" },
+        { team1: "X", team2: "X" }
+      ]
+    };
+  },
+  methods: {
+    assignNumberPairs: () => {
+      const team1Numbers = getRandomNumbers();
+      const team2Numbers = getRandomNumbers();
+      let returnValue = [];
+
+      for (let i = 0; i <= 9; i++) {
+        returnValue[i] = {
+          team1: team1Numbers[i],
+          team2: team2Numbers[i]
+        };
+      }
+      return returnValue;
+    }
+  }
+};
+
+const getRandomNumbers = () => {
+  const numberPool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const randomizedNumbers = [];
+  let randomIndex, randomNumber;
+
+  for (let i = 0; i <= 9; i++) {
+    randomIndex = Math.floor(Math.random() * (10 - i));
+    randomNumber = numberPool[randomIndex];
+
+    randomizedNumbers.push(randomNumber);
+    numberPool.splice(randomIndex, 1);
+  }
+
+  return randomizedNumbers;
+};
+</script>
---
+++ src/components/FootballSquares.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app id="football-squares">
+    <!-- <v-content> -->
+    <v-container>
+      <v-row align="center" justify="center">
+        <h1>Use this app to pick random numbers for the headers of your Football Squares grid</h1>
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-card-text>
+              <v-row>
+                <v-col class="team1-heading">Team 1</v-col>
+                <v-col class="team2-heading">Team 2</v-col>
+              </v-row>
+              <v-row v-for="(pair,index) in numberPairs" :key="index">
+                <v-col class="team1-numbers">{{pair.team1}}</v-col>
+                <v-col class="team2-numbers">{{pair.team2}}</v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-row align="center" justify="center">
+                <v-btn v-on:click="$emit('assign-numbers')" color="primary">Get Numbers</v-btn>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- </v-content> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    numberPairs: Array
+  }
+};
+</script>
+
+<style lang="scss">
+$team1-color: #d62027;
+$team2-color: #013369;
+
+h1 {
+  text-align: center;
+  font-size: 24px;
+}
+
+.row {
+  text-align: center;
+}
+
+.team1-heading {
+  color: $team1-color;
+  font-size: 18px;
+}
+
+.team2-heading {
+  color: $team2-color;
+  font-size: 18px;
+}
+
+.team1-numbers {
+  background-color: $team1-color;
+  color: #ffffff;
+  font-size: 18px;
+  border-right: 1px solid #ffffff;
+  padding: 6px !important;
+}
+
+.team2-numbers {
+  background-color: $team2-color;
+  color: #ffffff;
+  font-size: 18px;
+  padding: 6px !important;
+}
+</style>
